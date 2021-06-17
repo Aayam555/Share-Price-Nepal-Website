e6ba1528709b2d78b7cafbaa9a81daaa90ad338d
Aayam555 <63101665+Aayam555@users.noreply.github.com>
2021-06-17 19:27:42 +0545

    todayshareprice added

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify
-from nepse_scraper import Nepse
+from share_scraper import Share
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-data = Nepse()
+data = Share()
 
-@app.route('/api/shares')
-def shares_info():
+@app.route("/api/livestock")
+def live_stock():
     return jsonify(data.live_stock())
 
+@app.route("/api/todayshareprice")
+def today_share_price():
+    return jsonify(data.today_share_price())
 
-app.run(debug=True)
+app.run(host="0.0.0.0")
